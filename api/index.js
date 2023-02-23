fbf4830b9e2a33d80936b3c90bfd0c65fd1b124e
hesennov <hasanhasanov345@gmail.com>
2023-02-23 14:57:24 +0400

    son deyisiklikler yarim

--- api/index.js
+++ api/index.js
@@ -1,18 +1,17 @@
 import express from "express";
-// import authRoutes from './routes/auth.js'
-// import userRoutes from "./routes/user.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import postsRoutes from "./routes/posts.js";
 
 // const userRoutes = require("./routes/user")
-  
+
 const app = express();
 
 app.use(express.json());
 app.use("/api/posts", postsRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
-
-app.listen(3000, () => {
+app.listen(8800, () => {
   console.log("connected!");
 });
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    // console.log("asd");
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmut = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:8800/api/auth/register",
+        inputs
+      );
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input type="text" required placeholder="username" />
-        <input type="email" required placeholder="email" />
-        <input type="password" required placeholder="password" />
-        <button>Register</button>
+        <input
+          type="text"
+          required
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          required
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          required
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmut}>Register</button>
         <p>This is an error!</p>
         <span>
           Do you have an account? <Link to="/login">Register</Link>
